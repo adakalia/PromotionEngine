2be68d6f4337c2f52ed3fb5afb1f3804dc40bfa1
Anurag Dakaila <anurag.dakalia@cognizant.com>
2020-09-11 09:35:27 +0530

    Model Classes created

---
+++ PromotionEngine/PromotionEngine.Core/Items.cs
@@ -0,0 +1,10 @@
+﻿namespace PromotionEngine.Core
+{
+    public class Items
+    {
+        public char Sku{ get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ PromotionEngine/PromotionEngine.Core/Promotions.cs
@@ -0,0 +1,14 @@
+﻿namespace PromotionEngine.Core
+{
+    public class Promotions
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Details { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public char PrimarySku { get; set; }
+        public char SecondarySku { get; set; }
+
+    }
+}
